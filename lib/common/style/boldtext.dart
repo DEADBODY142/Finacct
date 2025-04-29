1773a3eb45c6e0a83e68ecbc833e61b6c587bc3a
Deadbody142 <keyrunshrestha619@gmail.com>
2025-04-29 15:06:27 +0545

    receipt_menu

--- lib/common/style/boldtext.dart
+++ lib/common/style/boldtext.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 class BoldText extends StatelessWidget {
   final String text;
   final Color? color;
+  final TextStyle? style;
 
-  const BoldText(this.text, {this.color, super.key});
+  const BoldText(this.text, {this.color, this.style, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        color: color ?? Colors.black,
+      style: (style ?? TextStyle()).copyWith(
+        fontWeight: FontWeight.bold, // Keep the font weight bold
+        color: color ?? Colors.black, // Default to black unless specified
       ),
     );
   }
