3e33a13a2546dea2b6df00886dfce24916cb17e3
Alisha Adhikari <elsaad5@gmail.com>
2025-04-29 15:00:15 +0545

    pull data

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:finacct/common/commonforall.dart';
 import 'package:finacct/demo.dart';
 import 'package:finacct/pages/login.dart';
 import 'package:finacct/pages/menu.dart';
+import 'package:finacct/pages/profile.dart';
 import 'package:finacct/pages/receipt.dart';
 import 'package:finacct/pages/receipt_info.dart';
 import 'package:finacct/pages/register.dart';
@@ -17,6 +18,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: ReceiptInfo());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Profile());
   }
 }
